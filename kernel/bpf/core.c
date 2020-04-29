536a67c3ea59573a7f13b7904887be1d6550a73f
Greg Kroah-Hartman <gregkh@google.com>
2020-04-29 15:16:49 +0200

    UPSTREAM: ANDROID: bpf: fix export symbol type
    In commit ff5bf35998cc ("ANDROID: bpf: validate bpf_func when BPF_JIT is
    enabled with CFI") a new symbol was exported, but it should have been
    set as a _GPL symbol.
    
    Fix this up by properly.
    
    Bug: 145210207
    Cc: Sami Tolvanen <samitolvanen@google.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>
    Change-Id: I7239bb8e0ef329cd7eac6afcd06c341b17ea680b

--- kernel/bpf/core.c
+++ kernel/bpf/core.c
@@ -625,6 +625,14 @@ static void bpf_jit_uncharge_modmem(u32 pages)
 	atomic_long_sub(pages, &bpf_jit_current);
 }
 
+#if IS_ENABLED(CONFIG_BPF_JIT) && IS_ENABLED(CONFIG_CFI_CLANG)
+bool __weak arch_bpf_jit_check_func(const struct bpf_prog *prog)
+{
+	return true;
+}
+EXPORT_SYMBOL_GPL(arch_bpf_jit_check_func);
+#endif
+
 struct bpf_binary_header *
 bpf_jit_binary_alloc(unsigned int proglen, u8 **image_ptr,
 		     unsigned int alignment,
@@ -651,6 +659,7 @@ bpf_jit_binary_alloc(unsigned int proglen, u8 **image_ptr,
 	/* Fill space with illegal/arch-dep instructions. */
 	bpf_fill_ill_insns(hdr, size);
 
+	bpf_jit_set_header_magic(hdr);
 	hdr->pages = pages;
 	hole = min_t(unsigned int, size - (proglen + sizeof(*hdr)),
 		     PAGE_SIZE - sizeof(*hdr));
@@ -1611,7 +1620,7 @@ static struct {
 	.null_prog = NULL,
 };
 
-struct bpf_prog_array __rcu *bpf_prog_array_alloc(u32 prog_cnt, gfp_t flags)
+struct bpf_prog_array *bpf_prog_array_alloc(u32 prog_cnt, gfp_t flags)
 {
 	if (prog_cnt)
 		return kzalloc(sizeof(struct bpf_prog_array) +
