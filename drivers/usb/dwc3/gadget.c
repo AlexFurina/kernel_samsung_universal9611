ff5503ab7c9be524f3feb6ed49d299218f3f37b7
Greg Kroah-Hartman <gregkh@google.com>
2022-07-21 21:55:35 +0200

    Merge 4.14.289 into android-4.14-stable
    Changes in 4.14.289
    	ALSA: hda - Add fixup for Dell Latitidue E5430
    	ALSA: hda/conexant: Apply quirk for another HP ProDesk 600 G3 model
    	xen/netback: avoid entering xenvif_rx_next_skb() with an empty rx queue
    	net: sock: tracing: Fix sock_exceed_buf_limit not to dereference stale pointer
    	ARM: 9213/1: Print message about disabled Spectre workarounds only once
    	ARM: 9214/1: alignment: advance IT state after emulating Thumb instruction
    	cgroup: Use separate src/dst nodes when preloading css_sets for migration
    	nilfs2: fix incorrect masking of permission flags for symlinks
    	net: dsa: bcm_sf2: force pause link settings
    	xhci: bail out early if driver can't accress host in resume
    	xhci: make xhci_handshake timeout for xhci_reset() adjustable
    	ARM: 9209/1: Spectre-BHB: avoid pr_info() every time a CPU comes out of idle
    	inetpeer: Fix data-races around sysctl.
    	net: Fix data-races around sysctl_mem.
    	cipso: Fix data-races around sysctl.
    	icmp: Fix data-races around sysctl.
    	ARM: dts: sunxi: Fix SPI NOR campatible on Orange Pi Zero
    	icmp: Fix a data-race around sysctl_icmp_ratelimit.
    	icmp: Fix a data-race around sysctl_icmp_ratemask.
    	ipv4: Fix data-races around sysctl_ip_dynaddr.
    	sfc: fix use after free when disabling sriov
    	seg6: fix skb checksum evaluation in SRH encapsulation/insertion
    	seg6: fix skb checksum in SRv6 End.B6 and End.B6.Encaps behaviors
    	sfc: fix kernel panic when creating VF
    	virtio_mmio: Add missing PM calls to freeze/restore
    	virtio_mmio: Restore guest page size on resume
    	netfilter: br_netfilter: do not skip all hooks with 0 priority
    	cpufreq: pmac32-cpufreq: Fix refcount leak bug
    	platform/x86: hp-wmi: Ignore Sanitization Mode event
    	net: tipc: fix possible refcount leak in tipc_sk_create()
    	NFC: nxp-nci: don't print header length mismatch on i2c error
    	net: sfp: fix memory leak in sfp_probe()
    	ASoC: ops: Fix off by one in range control validation
    	ASoC: wm5110: Fix DRE control
    	irqchip: or1k-pic: Undefine mask_ack for level triggered hardware
    	x86: Clear .brk area at early boot
    	signal handling: don't use BUG_ON() for debugging
    	USB: serial: ftdi_sio: add Belimo device ids
    	usb: dwc3: gadget: Fix event pending check
    	tty: serial: samsung_tty: set dma burst_size to 1
    	serial: 8250: fix return error code in serial8250_request_std_resource()
    	mm: invalidate hwpoison page cache page in fault path
    	can: m_can: m_can_tx_handler(): fix use after free of skb
    	Linux 4.14.289
    
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>
    Change-Id: I1e9b12a81151982c15f4a71b01aff2f1ad2eb7e5

--- drivers/usb/dwc3/gadget.c
+++ drivers/usb/dwc3/gadget.c
@@ -3832,9 +3832,6 @@ static irqreturn_t dwc3_process_event_buf(struct dwc3_event_buffer *evt)
 	}
 
 	evt->count = 0;
-#if IS_ENABLED(DWC3_GADGET_IRQ_ORG)
-	evt->flags &= ~DWC3_EVENT_PENDING;
-#endif
 	ret = IRQ_HANDLED;
 
 #if IS_ENABLED(DWC3_GADGET_IRQ_ORG)
@@ -3848,6 +3845,11 @@ static irqreturn_t dwc3_process_event_buf(struct dwc3_event_buffer *evt)
 		dwc3_writel(dwc->regs, DWC3_GEVNTCOUNT(0), DWC3_GEVNTCOUNT_EHB);
 		dwc3_writel(dwc->regs, DWC3_DEV_IMOD(0), dwc->imod_interval);
 	}
+
+	/* Keep the clearing of DWC3_EVENT_PENDING at the end */
+#if IS_ENABLED(DWC3_GADGET_IRQ_ORG)
+	evt->flags &= ~DWC3_EVENT_PENDING;
+#endif
 
 	return ret;
 }
